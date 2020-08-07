1b74dc81e2efbd91f0c03e11bfce3b7900b46c03
Gilbert Nwankwo <kellyemeka7@gmail.com>
2020-08-07 08:50:09 -0400

    extracted table header from movies table to its own reusable component

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,41 +1,26 @@
 import React, { Component } from "react";
 import Movie from "./movie";
+import TableHeader from "./common/tableHeader";
 
 class MoviesTable extends Component {
-
-  raiseSort = (path) => {
-    const sortColumn = { ...this.props.sortColumn };
-    if (path === sortColumn.path) {
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    } else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
-    this.props.onSort(sortColumn);
-  };
-
   render() {
-    const { movies, doDelete } = this.props;
+    const columns = [
+      { path: "title", label: "Title" },
+      { path: "genre.name", label: "Genre" },
+      { path: "numberInStock", label: "Stock" },
+      { path: "dailyRentalRate", label: "Rental" },
+      { path: "x" },
+      { path: "z" },
+    ];
+
+    const { movies, doDelete, onSort, sortColumn } = this.props;
     return (
       <table className="table">
-        <thead>
-          <tr>
-            <th onClick={() => this.raiseSort("title")} scope="col">
-              Title
-            </th>
-            <th onClick={() => this.raiseSort("genre.name")} scope="col">
-              Genre
-            </th>
-            <th onClick={() => this.raiseSort("numberInStock")} scope="col">
-              Stock
-            </th>
-            <th onClick={() => this.raiseSort("dailyRentalRate")} scope="col">
-              Rate
-            </th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
+        <TableHeader
+          columns={columns}
+          onSort={onSort}
+          sortColumn={sortColumn}
+        />
         <tbody>
           {movies.map((movie) => (
             <Movie key={movie._id} movie={movie} onDelete={doDelete} />
