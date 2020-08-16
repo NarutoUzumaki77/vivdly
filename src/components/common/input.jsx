0fdc4eb38b8c58702e50257ae97c70b0fe2a5c21
Gilbert Nwankwo <kellyemeka7@gmail.com>
2020-08-16 19:16:01 -0400

    created form component

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,22 +1,11 @@
 import React from "react";
 
-const InputField = ({ name, label, value, onChange, type, error }) => {
+const InputField = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        value={value}
-        onChange={onChange}
-        type={type}
-        className="form-control"
-        id={name}
-        name={name}
-      ></input>
-      {error && (
-        <div className="alert alert-danger" style={{ marginTop: "2px" }}>
-          {error}
-        </div>
-      )}
+      <input {...rest} className="form-control" id={name} name={name}></input>
+      {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
 };
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import InputField from "./common/input";
+import React from "react";
+import Form from "./common/form";
 import Joi from "joi-browser";
 
-class LoginForm extends Component {
+class LoginForm extends Form {
   constructor(props) {
     super(props);
     this.state = {
-      account: {
+      data: {
         username: "",
         password: "",
       },
@@ -19,70 +19,23 @@ class LoginForm extends Component {
     password: Joi.string().required().label("Password"),
   };
 
-  validate = () => {
-    const options = { abortEarly: false };
-    const { error } = Joi.validate(this.state.account, this.schema, options);
-    if (!error) return null;
-
-    const errors = {};
-    for (let item of error.details) errors[item.path[0]] = item.message;
-    return errors;
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-
+  doSubmit = () => {
     // call server
     console.log("Submitted");
   };
 
-  validateProperty = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema);
-
-    return error ? error.details[0].message : null;
-  };
-
-  handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-
-    this.setState({ account, errors });
-  };
-
   render() {
-    const { account, errors } = this.state;
 
     return (
       <form onSubmit={this.handleSubmit}>
         <h1>Login</h1>
-        <InputField
-          name="username"
-          value={account.username}
-          label="Username"
-          type="text"
-          onChange={this.handleChange}
-          error={errors.username}
-        />
-        <InputField
-          name="password"
-          value={account.password}
-          label="Password"
-          type="password"
-          onChange={this.handleChange}
-          error={errors.password}
-        />
-        <button type="submit" className="btn btn-primary">
+        {this.renderInput('username', 'Username')}
+        {this.renderInput('password', 'Password', 'password')}
+        <button
+          disabled={this.validate()}
+          type="submit"
+          className="btn btn-primary"
+        >
           Submit
         </button>
       </form>
